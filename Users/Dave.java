42f016c107e2402f36c10bda2f6e6d6feeb59b54
Tristan <tristan@tristanwiley.com>
2016-01-06 23:35:00 -0500

    Added Dave

---
+++ Users/Dave.java
@@ -0,0 +1,22 @@
+public class Dave{
+
+	boolean isRoomOwner = false;
+	boolean hasCircle = true;
+
+	public Dave() {
+		// TODO auto generated method stub
+	}
+
+	public boolean isCircle(){
+		return hasCircle;
+	}
+
+	public boolean isOwner(){
+		return isRoomOwner;
+	}
+	
+	public String whatArePeople(){
+		return "Tired of Dave's Shit";
+	}
+	
+}
