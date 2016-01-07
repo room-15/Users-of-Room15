073da22320fb535931ae5f92569c31484308ba77
Tristan <tristan@tristanwiley.com>
2016-01-06 22:50:26 -0500

    Added Adam
    <3 Adam

---
+++ Users/Adam.java
@@ -0,0 +1,16 @@
+public class Adam{
+
+	boolean isRoomOwner = true;
+	boolean isDumbass = true;
+
+	//Number of times Adam has lost his keystore
+	int keystoreLost = 1;
+
+	public boolean hasLostKeystore(){
+		return true;
+	}
+
+	public int keystoreLostCount(){
+		return keystoreLost;
+	}
+}
