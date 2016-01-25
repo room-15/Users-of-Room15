96a65b09de0daa44b1a7a2718941d47ef792fcf4
Maurício Pessoa <mauricio.ufma@gmail.com>
2016-01-25 13:50:04 -0300

    Updated Mauker;

--- Users/Mauker.java
+++ Users/Mauker.java
@@ -3,9 +3,16 @@ public class Mauker extends Room15User {
 	boolean isRoomOwner = false;
 	boolean hasCircle = true;
 	boolean hasAndroidWear = true;
+	boolean hasHat = true;
+	ArrayList<Color> hatColors;
 
 	public Mauker() {
 		// TODO auto generated method stub
+		if (hasHat) {
+			hatColors = new ArrayList<Color>();
+			hatColors.add(Color.BLACK);
+			hatColors.add(Color.WHITE);
+		}	
 	}
 
 	public boolean isCircle(){
@@ -18,6 +25,19 @@ public class Mauker extends Room15User {
 
 	public boolean hasAndroidWear() {
 		return hasAndroidWear;
+	}
+
+	public boolean hasHat() {
+		return hasHat;
+	}
+
+	public ArrayList<Color> getHatColors() {
+		if (hasHat) {
+			if (hatColors != null) {
+				return hatColors;
+			}
+		}
+		return null;
 	}
 
 	@Override
