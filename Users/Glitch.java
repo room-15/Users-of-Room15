5d21db82d6d0f2c2d4db66370361d17d5c864553
Maurício Pessoa <mauricio.ufma@gmail.com>
2016-01-25 13:39:35 -0300

    Indentation of Glitch;

--- Users/Glitch.java
+++ Users/Glitch.java
@@ -1,27 +1,27 @@
 {
-{	
-;ǝnɹʇ uɹnʇǝɹ		
-} ()uʍopǝpᴉsd∩sᴉ uɐǝlooq ɔᴉlqnd	
+	{	
+		;ǝnɹʇ uɹnʇǝɹ		
+	} ()uʍopǝpᴉsd∩sᴉ uɐǝlooq ɔᴉlqnd	
 
-{	
-;ssɐqɯnpsᴉ uɹnʇǝɹ		
-} ()ssɐqɯnpsᴉ uɐǝlooq ɔᴉlqnd	
+	{	
+		;ssɐqɯnpsᴉ uɹnʇǝɹ		
+	} ()ssɐqɯnpsᴉ uɐǝlooq ɔᴉlqnd	
 
-{	
-;ɹǝuʍOɯooɹsᴉ uɹnʇǝɹ		
-}()ɹǝuʍOsᴉ uɐǝlooq ɔᴉlqnd	
+	{	
+		;ɹǝuʍOɯooɹsᴉ uɹnʇǝɹ		
+	}()ɹǝuʍOsᴉ uɐǝlooq ɔᴉlqnd	
 
-{	
-;ǝslɐɟ : ǝnɹʇ ¿ ǝlɔɹᴉƆsɐɥ uɹnʇǝɹ		
-}()ǝlɔɹᴉƆsᴉ uɐǝlooq ɔᴉlqnd	
+	{	
+		;ǝslɐɟ : ǝnɹʇ ¿ ǝlɔɹᴉƆsɐɥ uɹnʇǝɹ		
+	}()ǝlɔɹᴉƆsᴉ uɐǝlooq ɔᴉlqnd	
 
-{	
-		
-} ()ɥɔʇᴉlפ ɔᴉlqnd	
+	{	
+			
+	} ()ɥɔʇᴉlפ ɔᴉlqnd	
 
-;ǝnɹʇ = uʍopǝpᴉsd∩sᴉ uɐǝlooq ǝʇɐʌᴉɹd	
-;ǝslɐɟ = ǝlɔɹᴉƆsɐɥ uɐǝlooq ǝʇɐʌᴉɹd	
-;ǝslɐɟ = ssɐqɯnpsᴉ uɐǝlooq ǝʇɐʌᴉɹd	
-;ǝslɐɟ = ɹǝuʍOɯooɹsᴉ uɐǝlooq ǝʇɐʌᴉɹd	
+    ;ǝnɹʇ = uʍopǝpᴉsd∩sᴉ uɐǝlooq ǝʇɐʌᴉɹd	
+    ;ǝslɐɟ = ǝlɔɹᴉƆsɐɥ uɐǝlooq ǝʇɐʌᴉɹd	
+    ;ǝslɐɟ = ssɐqɯnpsᴉ uɐǝlooq ǝʇɐʌᴉɹd	
+    ;ǝslɐɟ = ɹǝuʍOɯooɹsᴉ uɐǝlooq ǝʇɐʌᴉɹd	
 
-} ɹǝs∩ϛƖɯooɹ spuǝʇxǝ ɥɔʇᴉlפ ssɐlɔ ɔᴉlqnd
+        } ɹǝs∩ϛƖɯooɹ spuǝʇxǝ ɥɔʇᴉlפ ssɐlɔ ɔᴉlqnd
