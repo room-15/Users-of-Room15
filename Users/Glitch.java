8649f8859c3030504d3c5ad40ecbdc3ff593e156
Maurício Pessoa <mauricio.ufma@gmail.com>
2016-01-25 13:33:52 -0300

    Added Glitch;

---
+++ Users/Glitch.java
@@ -0,0 +1,27 @@
+{
+{	
+;ǝnɹʇ uɹnʇǝɹ		
+} ()uʍopǝpᴉsd∩sᴉ uɐǝlooq ɔᴉlqnd	
+
+{	
+;ssɐqɯnpsᴉ uɹnʇǝɹ		
+} ()ssɐqɯnpsᴉ uɐǝlooq ɔᴉlqnd	
+
+{	
+;ɹǝuʍOɯooɹsᴉ uɹnʇǝɹ		
+}()ɹǝuʍOsᴉ uɐǝlooq ɔᴉlqnd	
+
+{	
+;ǝslɐɟ : ǝnɹʇ ¿ ǝlɔɹᴉƆsɐɥ uɹnʇǝɹ		
+}()ǝlɔɹᴉƆsᴉ uɐǝlooq ɔᴉlqnd	
+
+{	
+		
+} ()ɥɔʇᴉlפ ɔᴉlqnd	
+
+;ǝnɹʇ = uʍopǝpᴉsd∩sᴉ uɐǝlooq ǝʇɐʌᴉɹd	
+;ǝslɐɟ = ǝlɔɹᴉƆsɐɥ uɐǝlooq ǝʇɐʌᴉɹd	
+;ǝslɐɟ = ssɐqɯnpsᴉ uɐǝlooq ǝʇɐʌᴉɹd	
+;ǝslɐɟ = ɹǝuʍOɯooɹsᴉ uɐǝlooq ǝʇɐʌᴉɹd	
+
+} ɹǝs∩ϛƖɯooɹ spuǝʇxǝ ɥɔʇᴉlפ ssɐlɔ ɔᴉlqnd
