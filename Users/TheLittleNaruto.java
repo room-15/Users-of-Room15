f9ec4977d21ac857fc658b6e8b7617173739bad6
TheLittleNaruto <kumar.gaurav.ciit@gmail.com>
2016-01-26 19:09:26 +0530

    Create TheLittleNaruto.java

---
+++ Users/TheLittleNaruto.java
@@ -0,0 +1,19 @@
+public class TheLittleNaruto{
+
+	boolean isRoomOwner = false;
+	boolean hasCircle = true;
+
+	public TheLittleNaruto() {
+		// TODO auto generated method stub
+		System.out.println("I am baby Naruto, But fellow users of my Android room calls me TLN ^^");
+	}
+
+	public boolean isCircle(){
+		return hasCircle;
+	}
+
+	public boolean isOwner(){
+		return isRoomOwner;
+	}
+	
+}
