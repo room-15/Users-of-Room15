8903ceada6a799a5649af601b2fc4a7cadac365e
LinX64 <ash.wxrz@hotmail.com>
2016-01-26 18:37:19 +0330

    Create LinX64.java

---
+++ Users/LinX64.java
@@ -0,0 +1,19 @@
+public class LinX64 extends Room15User {
+
+    boolean isRoomOwner = false;
+    boolean hasCircle = true;
+
+    public LinX64() {
+        // TODO auto generated method stub
+        System.out.println("My name is 'Mohsen' called LinX64, an Android Developer.");
+    }
+
+    public boolean isCircle(){
+        return hasCircle;
+    }
+
+    public boolean isOwner(){
+        return isRoomOwner;
+    }
+
+}
