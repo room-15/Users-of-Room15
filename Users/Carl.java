0b12a3f0088057a5b952a688e580cdab4092840e
Tristan Wiley <trwiley@twcny.rr.com>
2016-01-27 13:57:37 -0500

    Carl
    Carl is deprecated, also added isCircle and isOwner

--- Users/Carl.java
+++ Users/Carl.java
@@ -1,9 +1,21 @@
+@Deprecated
 public class Carl extends Room15User {
+  private boolean isRoomOwner = true;
+	private boolean hasCircle = true;
+
   public Carl() {
-  
+    System.out.println("Warning: The class Carl is deprecated.  It has been in use for a long time and is too old to use");
   }
   
   public int getAge() {
     return Integer.MAX_VALUE;
   }
+  
+  public boolean isCircle(){
+		return hasCircle;
+	}
+
+	public boolean isOwner(){
+		return isRoomOwner;
+	}
 }
