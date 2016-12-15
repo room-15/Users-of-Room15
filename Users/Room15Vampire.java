27b345451e59fea3c9ab9aa49771e0d8f9bbd634
Rupinderjeet Singh Hans <rupinderjeet@users.noreply.github.com>
2016-12-15 11:10:03 +0530

    Update Room15Vampire.java

--- Users/Room15Vampire.java
+++ Users/Room15Vampire.java
@@ -1,56 +1,61 @@
 package Users;
 
-public class Room15Vampire extends Room15User{
+public class Room15Vampire extends Room15User {
 
 	// TODO : create cure();
 	
-	public Interface VampireInterface(){
+	public Interface VampireInterface() {
 		void onHelpReceived(String resource);
 		void onHelpReceiveTimeout(String resource);
 	}
 	
 	// initiates basic capabilities
-	public Room15Vampire(String name){
+	public Room15Vampire(String name) {
 		/* compiled code */
 	}
 	
 	// posts received resources, waits for response, fires VampireInterface methods
-	public void ask(String question){
+	public void ask(String question) {
 		/* compiled code */
 	}
 	
 	// consumes the life force of resource
-	public void drink(String resource){
+	public void drink(String resource) {
 		/* compiled code */
 	}
 	
 	// sets received resource to null
-	public void dodge(String resource){
+	public void dodge(String resource) {
 		/* compiled code */
 	}
 	
 	// fake Thread.sleep
-	public void goWandering(long millis){
+	public void goWandering(long millis) {
 		/* compiled code */
 	}
 	
-	public void bite(Room15Vampire victim){
+	public void bite(Room15Vampire victim) {
 		/* compiled code */
 	}
 	
-	public boolean isVampire(Room15User userToCheck){
+	public boolean isVampire(Room15User userToCheck) {
 		/* compiled code */
 	}
 	
-	public void promoteToHelpVampire(Room15Vampire vampire){
+	public void promoteToHelpVampire(Room15Vampire vampire) {
 		/* compiled code */
 	}
 	
-	public void promoteToRealmVampire(Room15Vampire vampire){
+	public void promoteToComplainVampire(Room15Vampire vampire) {
 		/* compiled code */
 	}
 
-	public void promoteRealm(String realmUrl){
+	public void fileComplains(String[] complains) {
 		/* compiled code */
+	}
+	
+	// lasts 12 hours, cooldown 12 hours
+	public void setHumanForm(Room15Vampire vampire) {
+		/* compiledcode */
 	}
 }
